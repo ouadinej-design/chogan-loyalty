8f14bf0238af4606f1049b0646773e27f3a77444
ouadinej-design <ouadi.nej@gmail.com>
2026-05-09 09:49:59 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Chogan Loyalty - Premium",
+  description: "Application de fidélité Chogan",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
